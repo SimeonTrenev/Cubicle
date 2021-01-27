36f7ad2ce15178b1e49ee8993bb8921df181985f
Simeon Trenev <64967770+Frontab@users.noreply.github.com>
2021-01-27 16:42:11 +0200

    add controllers

---
+++ controllers/productController.js
@@ -0,0 +1,22 @@
+const { Router } = require("express");
+const router = Router();
+
+
+//First option to routh
+// const index = (req, res) => {
+//   res.render("home", { layout: false });
+// };
+
+// router.get("/", index);
+
+//Secont option to routh
+
+router.get('/', (req, res) => {
+    res.render('home', {layout: false})
+})
+
+router.get('/create', (req, res) => {
+    res.render('create', {layout: false})
+})
+
+module.exports = router;
--- routes.js
+++ routes.js
@@ -2,10 +2,19 @@
 // const Router = require('express').Router;
 
 const express = require('express')
+
 const router = express.Router();
 
-router.get('/', (req, res) => {
-    res.render('home', {layout : false})
-})
+const productController = require('./controllers/productController');
+
+const aboutController = require('./controllers/aboutController')
+
+//First option
+
+// router.get('/', productController.index);
+// router.get('/create', productController.create);
+
+router.use('/', productController);
+router.use('/about', aboutController);
 
 module.exports = router;
