adb616bfe4efa6c684eb3b8ec17f5424b2541a5a
Simeon Trenev <64967770+Frontab@users.noreply.github.com>
2021-01-28 17:39:13 +0200

    save data to json file

--- controllers/productController.js
+++ controllers/productController.js
@@ -19,12 +19,13 @@ router.get("/create", (req, res) => {
   res.render("create", { title: "Create" });
 });
 
-router.post("/create", (req, res) => {
+router.post("/create",  (req, res) => {
   // console.log(req.body)
 
   //TODO : Validate inputs
 
   productService.create(req.body)
+  // productService.validateCube(req.body)
 
   res.redirect("/products");
 });
@@ -33,5 +34,7 @@ router.get("/details/:productId", (req, res) => {
   console.log(req.params);
   res.render("details", { title: "Product Details" });
 });
+
+
 
 module.exports = router;
--- services/productService.js
+++ services/productService.js
@@ -1,6 +1,11 @@
 const Cube = require("../models/Cube");
 const uniqId = require("uniqid");
+const fs = require('fs')
+let productsData = require('../config/products.json')
 
+function getAll(){
+  return productsData;
+}
 
 
 function create(data) {
@@ -8,9 +13,38 @@ function create(data) {
 
   let cube = new Cube(uniqId(), name, description, imageUrl, difficultyLevel);
 //   console.log(cube);
+ 
+
+productsData.push(cube)
+
+fs.writeFile(__dirname +  '/../config/products.json', JSON.stringify(productsData), (err) => {
+  if(err){
+    console.log(err)
+    return;
+  }
+
+
+})
 }
+
+// function validateCube(data){
+//   if(data.name.length < 2){
+//     return;
+//   }
+
+//   if(data.description.length < 5){
+//     return;
+//   }
+
+//   if(!data.imageUrl){
+//     return
+//   }
+
+  
+// }
 
 
 module.exports = {
-    create
+    create,
+    validateCube
 }
