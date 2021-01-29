986b3eae1099119a19336a77d27d6cc71a1e71dd
Simeon Trenev <64967770+Frontab@users.noreply.github.com>
2021-01-29 13:52:06 +0200

    add product lair

--- services/productService.js
+++ services/productService.js
@@ -1,32 +1,37 @@
 const Cube = require("../models/Cube");
 const uniqId = require("uniqid");
-const fs = require("fs");
+// const fs = require("fs");
 // const fs = require("fs/promises"); authomatic promises
 // const fs = require('fs').promises // for my old version of node.js
 
-let productsData = require("../config/products.json");
-const path = require("path");
+
+// const path = require("path");
+const productData = require('../data/productsData')
 
 function getAll(query) {
-  let result = productsData;
+  let products = productData.getAll();
 
-  if(query.search){
-    result = result.filter(x => x.name.toLowerCase().includes(query.search))
+  const { search, from, to } = query
+
+  if (search) {
+    products = products.filter((x) => x.name.toLowerCase().includes(search));
   }
 
-  if(query.from){
-    result = result.filter(x => Number(x.level) >= Number(query.from))
+  if (from) {
+    products = products.filter((x) => Number(x.level) >= Number(from));
   }
 
-  if(query.to){
-    result = result.filter(x => Number(x.level) <= Number(query.to))
+  if (to) {
+    products = products.filter((x) => Number(x.level) <= Number(to));
   }
 
-  return result;
+  return products;
 }
 
 function getOne(id) {
-  return productsData.find((x) => x.id == id);
+  // return productsData.find((x) => x.id == id);
+
+  return productData.getOne(id)
 }
 
 function create(data, callback) {
@@ -35,25 +40,26 @@ function create(data, callback) {
   let cube = new Cube(uniqId(), name, description, imageUrl, difficultyLevel);
   //   console.log(cube);
 
-  productsData.push(cube);
+  // productsData.push(cube);
 
   // console.log(path.join(__dirname + "../config/products.json"))
   // console.log(path.resolve("../config/products.json"))
 
-  fs.writeFile(
-    path.join(__dirname, "../config/products.json"),
-    JSON.stringify(productsData),
-    callback
-    // (err) => {
-    //   if (err) {
-    //     console.log(err);
-    //     return;
-    //   }
+  // fs.writeFile(
+  //   path.join(__dirname, "../config/products.json"),
+  //   JSON.stringify(productsData),
+  //   callback
+  //   // (err) => {
+  //   //   if (err) {
+  //   //     console.log(err);
+  //   //     return;
+  //   //   }
 
-    //   //   callback()
-    //   // }
-  );
+  //   //   //   callback()
+  //   //   // }
+  // );
 
+  return productData.create(cube)
   //Promise authomatic
   //  return fs.writeFile(path.join(__dirname, "../config/products.json") ,
   //   JSON.stringify(productsData),
